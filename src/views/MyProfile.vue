a16f0f4e39d3185cc2d06ba8e157b5300c8d5a9f
ganlinlaomu <101681744+ganlinlaomu@users.noreply.github.com>
2026-09-26 12:50:25 +0900

    Preserve compact profile header behavior

--- src/views/MyProfile.vue
+++ src/views/MyProfile.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="profile-page app-page">
-    <SecondaryPageHeader title="我的资料" back-label="返回我的" />
+    <SecondaryPageHeader title="我的资料" back-label="返回" variant="compact" />
 
     <section class="profile-editor">
       <form @submit.prevent="save">
